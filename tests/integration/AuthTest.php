bb2b37f386aa0a1a49faa62bde69cebfba3f4106
Vladimir Romanichev <pro100print@yahoo.com>
2022-06-07 16:42:47 +0300

    write auth test and todo

---
+++ tests/integration/AuthTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Otis22\VetmanagerRestApi\integration;
+
+use GuzzleHttp\Client;
+use Otis22\VetmanagerRestApi\Headers;
+use PHPUnit\Framework\TestCase;
+
+use function Otis22\VetmanagerRestApi\byApiKey;
+use function Otis22\VetmanagerRestApi\byServiceApiKey;
+use function Otis22\VetmanagerRestApi\uri;
+use function Otis22\VetmanagerUrl\url;
+
+final class AuthTest extends TestCase
+{
+    public function testApiKey(): void
+    {
+        $this->assertTrue(
+            $this->jsonTestResponse(
+                byApiKey(
+                    (string) getenv("TEST_API_KEY")
+                )
+            )->success
+        );
+    }
+// TODO: Find a way fot test it before feature will be released.
+/*    public function testServiceApiKey(): void
+    {
+        $this->assertTrue(
+            $this->jsonTestResponse(
+                byServiceApiKey(
+                    (string) getenv('SERVICE_NAME'),
+                    (string) getenv('SERVICE_API_KEY')
+                )
+            )->success
+        );
+    }
+*/
+    public function jsonTestResponse(Headers $auth): \stdClass
+    {
+        $client = new Client(
+            [
+                'base_uri' => url(
+                    (string) getenv('TEST_DOMAIN_NAME')
+                )->asString()
+            ]
+        );
+        $request = $client->request(
+            'GET',
+            uri('clinics')->asString(),
+            [
+                'headers' => $auth->asKeyValue()
+            ]
+        );
+        return json_decode(
+            (string) $request->getBody(),
+            false,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+    }
+}
